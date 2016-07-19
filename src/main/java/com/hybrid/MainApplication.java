2f0c42605fb50abb2821bf5703b851ab54979e23
java <java>
2016-07-19 16:28:57 +0900

    10ea Hello & Label change...

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -4,8 +4,9 @@ public class MainApplication {
 
    public static void main(String[] args) {
       System.out.println("##############");
-      System.out.println("##############");
-      System.out.println("Hello FirstGradle");
+      System.out.println("@@@@@@@@@@@@@@");
+      for (int i=0; i<10; i++)	
+      System.out.println("Hello FirstGradle" + i);
       System.out.println("##############");
       System.out.println("##############");
    }
