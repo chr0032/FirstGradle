c45ebc1a4fc991437167c54c9f1c54c3e19a3ce6
java <java>
2016-07-19 16:22:45 +0900

    Hello Label add...

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -4,7 +4,9 @@ public class MainApplication {
 
    public static void main(String[] args) {
       System.out.println("##############");
+      System.out.println("##############");
       System.out.println("Hello FirstGradle");
+      System.out.println("##############");
       System.out.println("##############");
    }
 }
